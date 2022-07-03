73258ea581f788997a78c4bfe79ae61104c27bda
Alex <mbr55aa@gmail.com>
2022-07-03 14:43:23 +0000

    Add settings

---
+++ library_management/library_management/doctype/library_settings/library_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Alex N and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Library Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
